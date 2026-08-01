fe79692419c0d9cf569ab4fe2d17b1f895d0b964
Sylvester Nyathi <Sylvesternyathi566@gmail.com>
2026-08-01 22:10:32 +0200

    feat: Phase 24 - Full Multi-Timeframe Stack

--- src/slytrade/strategies/__init__.py
+++ src/slytrade/strategies/__init__.py
@@ -1,4 +1,4 @@
 from .baselines import ICTBiasBaselineStrategy, ICTConfluenceStrategy
 from .mtf_confluence import MTFICTConfluenceStrategy
 
-__all__ = ["ICTBiasBaselineStrategy", "ICTConfluenceStrategy", "MTFICTConfluenceStrategy"]
+__all__ = ["ICTConfluenceStrategy", "ICTBiasBaselineStrategy", "MTFICTConfluenceStrategy"]
